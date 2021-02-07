678b0bafb1cca8c6435dd7ca13a0e3a8bdab8c96
davidch2020 <63265252+davidch2020@users.noreply.github.com>
2021-02-07 10:30:01 -0500

    Update Card.java

--- Card.java
+++ Card.java
@@ -6,20 +6,35 @@ public class Card {
     int[] computerCards = new int[5];
     int[] computerSuites = new int[5];
     int randomNum;
+    boolean stillLooking = true;
 
     Random randomNumGenerator = new Random();
 
     public Card() {
-        for (int i = 0; i < playerCards.length; i++) {
-            randomNum = randomNumGenerator.nextInt(13) + 1;
-            playerCards[i] = randomNum;
-            randomNum = randomNumGenerator.nextInt(13) + 1;
-            computerCards[i] = randomNum;
-            randomNum = randomNumGenerator.nextInt(4) + 1;
-            playerSuites[i] = randomNum;
-            randomNum = randomNumGenerator.nextInt(4) + 1;
-            computerSuites[i] = randomNum;
-        }    
+       for (int i = 0; i < playerCards.length; i++) {
+            createRandomNum(i);
+        }   
+    }
+    
+    void createRandomNum(int i) {
+        int cardRandomNum = 0;
+        int suitesRandomNum = 0;
+        
+        while (stillLooking) {
+            cardRandomNum = randomNumGenerator.nextInt(13) + 1;
+            //playerCards[i] = cardRandomNum;
+            suitesRandomNum = randomNumGenerator.nextInt(4) + 1;
+            //playerSuites[i] = suitesRandomNum;
+            stillLooking = false;
+            for (int x = 0; x < 5; x++) {
+                if (playerCards[x] == cardRandomNum && playerSuites[x] == suitesRandomNum) {
+                    stillLooking = true;
+                } 
+            }
+        }
+
+        playerCards[i] = cardRandomNum;
+        playerSuites[i] = suitesRandomNum;
     }
 
 }
